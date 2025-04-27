408be4ec9def816866a60dbcc101046d9d11835e
RazaDon Willrich <razradical93@gmail.com>
2025-04-27 22:57:14 +0000

    Completed Spring JPA Multiplicity.

--- src/main/java/Application/Service/StudentService.java
+++ src/main/java/Application/Service/StudentService.java
@@ -59,6 +59,13 @@ public class StudentService {
      */
     public void assignClassroomToStudent(long studentId, Classroom classroom){
 
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            student.setClassroom(classroom);
+            studentRepository.save(student);
+        }
     }
 
     /**
@@ -68,6 +75,13 @@ public class StudentService {
      * @return the Classroom of the student
      */
     public Classroom getClassroomOfStudent(long studentId){
+
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            return student.getClassroom();
+        }
         return null;
     }
 
@@ -79,5 +93,12 @@ public class StudentService {
      */
     public void unassignClassroomOfStudent(long studentId){
 
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            student.setClassroom(null);
+            studentRepository.save(student);
+        }
     }
 }
